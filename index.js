76bc96d9b89e502dbe2f45b370523a76ee34ea3a
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-03-26 21:49:18 +0000

    feat: add css reset and some basic styling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x1a1b26);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
@@ -8,7 +9,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: 0x73daca });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
